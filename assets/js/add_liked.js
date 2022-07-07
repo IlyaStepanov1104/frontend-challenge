06882a459f9c5fbdf7da748621b1abe0fe6f7794
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-07-07 23:45:10 +0300

    delete border in hearts, add a filling like.html

--- assets/js/add_liked.js
+++ assets/js/add_liked.js
@@ -1,8 +1,10 @@
-var cookie = document.cookie.split("=liked;");
-cookie[cookie.length - 1] = cookie[cookie.length - 1].substring(0, cookie[cookie.length - 1].length-6)
+//var cookie = document.cookie.split("=liked;");
+//cookie[cookie.length - 1] = cookie[cookie.length - 1].substring(0, cookie[cookie.length - 1].length-6)
+cookie = ["https://cdn2.thecatapi.com/images/at0.jpg", "https://cdn2.thecatapi.com/images/2d8.jpg"]
 for (var i = 0; i < cookie.length; i++) {
 	let div = document.createElement('div');
 	div.style.background = "url(" + cookie[i] + ")";
-	div.addClass = "img";
+	div.className = "img";
+	div.innerHTML = '<button class="liked" id="' + cookie[i] + '"></button>';
 	liked_block.append(div);
 }
