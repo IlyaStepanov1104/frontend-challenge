f4d2d4eb2ac9d4b27f0620bb78d73164f00b6301
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-07-07 23:46:36 +0300

    connecting to cookies

--- assets/js/add_liked.js
+++ assets/js/add_liked.js
@@ -1,6 +1,5 @@
-//var cookie = document.cookie.split("=liked;");
-//cookie[cookie.length - 1] = cookie[cookie.length - 1].substring(0, cookie[cookie.length - 1].length-6)
-cookie = ["https://cdn2.thecatapi.com/images/at0.jpg", "https://cdn2.thecatapi.com/images/2d8.jpg"]
+var cookie = document.cookie.split("=liked;");
+cookie[cookie.length - 1] = cookie[cookie.length - 1].substring(0, cookie[cookie.length - 1].length-6)
 for (var i = 0; i < cookie.length; i++) {
 	let div = document.createElement('div');
 	div.style.background = "url(" + cookie[i] + ")";
