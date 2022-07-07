bf5cde5585c14ab604de4b9b9ac58a76015ee1a2
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-07-07 22:34:11 +0300

    add save in cookie

--- assets/js/like.js
+++ assets/js/like.js
@@ -1,11 +1,11 @@
 function like(block){
 	if(block.classList.contains('heart')){
+		document.cookie = block.id + "=liked;max-age=31556926";
 		block.className = 'liked'
+		console.log(document.cookie);
 	} else if (block.classList.contains('liked')){
 		block.className = 'heart'
 	}
 }
 
-document.cookie = "name=user;max-age=31556926";
-console.log(document.cookie);
 document.addEventListener('click', e => like(e.target))
