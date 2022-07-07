514ff5a36e6eb422e68f280f0b3b4b7e1debc140
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-07-07 23:22:40 +0300

    delete '=liked;' in the end

--- assets/js/add_liked.js
+++ assets/js/add_liked.js
@@ -1,4 +1,5 @@
 var cookie = document.cookie.split("=liked;");
+cookie[cookie.length - 1].splice(-6, 6)
 for (var i = 0; i < cookie.length; i++) {
 	let div = document.createElement('div');
 	div.addClass = "img";
