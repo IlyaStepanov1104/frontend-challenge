e3fe372252f64dd470736dc9a3605a8eeb835835
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-07-07 22:38:54 +0300

    add cookie clearing and deletion of the record when
    of the record when you like it again

--- assets/js/like.js
+++ assets/js/like.js
@@ -2,10 +2,18 @@ function like(block){
 	if(block.classList.contains('heart')){
 		document.cookie = block.id + "=liked;max-age=31556926";
 		block.className = 'liked'
-		console.log(document.cookie);
 	} else if (block.classList.contains('liked')){
+		document.cookie = block.id + "liked;max-age=-1";
 		block.className = 'heart'
 	}
+	console.log(document.cookie);
+}
+
+// DELETE ALL COOKIES
+var cookies = document.cookie.split(/;/);
+for (var i = 0, len = cookies.length; i < len; i++) {
+	var cookie = cookies[i].split(/=/);
+	document.cookie = cookie[0] + "=;max-age=-1";
 }
 
 document.addEventListener('click', e => like(e.target))
