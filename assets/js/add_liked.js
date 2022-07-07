e523941b4f3483f0f5db2a1538923e975f2fd98b
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-07-07 23:28:53 +0300

    add background for like

--- assets/js/add_liked.js
+++ assets/js/add_liked.js
@@ -3,6 +3,6 @@ cookie[cookie.length - 1] = cookie[cookie.length - 1].substring(0, cookie[cookie
 for (var i = 0; i < cookie.length; i++) {
 	let div = document.createElement('div');
 	div.addClass = "img";
-	div.innerHTML = cookie[i];
+	div.style.background = "url(" + cookie[i] + ")";
 	liked_block.append(div);
 }
